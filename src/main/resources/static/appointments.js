0a357b51d15095d08469a739b45c64f270f9c1ec
Razvan Turc <razvanturc.biz@gmain.com>
2022-04-20 18:55:40 +0300

    fixed some jQuery code

--- src/main/resources/static/appointments.js
+++ src/main/resources/static/appointments.js
@@ -1,43 +1,5 @@
 $(document).ready(() => {
-
-    // $('#add-appointment-button').click(function () {
-    //     const appointmentId = this.parentElement.id;
-    //     const time = $('#modal-time option:selected').text();
-    //     const pet = $('#modal-pet').val();
-    //     const owner = $('#modal-owner').val();
-    //     const vet = $('#modal-vet').val();
-    //
-    //     const newAppointment = {
-    //         id: appointmentId,
-    //         time: time,
-    //         vet: vet,
-    //         pet: pet,
-    //         owner: owner
-    //     };
-    //
-    //     alert(JSON.stringify({
-    //         appointmentId,
-    //         time,
-    //         pet,
-    //         owner,
-    //         vet
-    //     }));
-    //
-    //     fetch('appointment/' + appointmentId, {
-    //         method: 'PUT',
-    //         body: JSON.stringify(newAppointment),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     }).then(response => {
-    //         if (response.ok) {
-    //             location.reload();
-    //         } else {
-    //             alert("There are errors " + response.status)
-    //         }
-    //     });
-    // });
-
+    // ADD - UPDATE
     $('.add-icon').click(function () {
         const appointmentId = this.parentElement.id;
         const row = this.parentElement.parentElement;
@@ -64,8 +26,8 @@ $(document).ready(() => {
                 vet
             }));
 
-            fetch('appointment/', {
-                method: 'POST',
+            fetch('appointment/' + appointmentId, {
+                method: 'PUT',
                 body: JSON.stringify(newAppointment),
                 headers: {
                     'Content-Type': 'application/json'
@@ -80,29 +42,29 @@ $(document).ready(() => {
         });
     });
 
+// EDIT
+//     $('.edit-icon').click(function () {
+//         const appointmentId = this.parentElement.id;
+//         const row = this.parentElement.parentElement;
+//         const pet = row.children[1].innerText;
+//         const owner = row.children[2].innerText;
+//         const vet = row.children[3].innerText;
+//
+//         // const newAppointment = {
+//         //     id : appointmentId,
+//         //     pet : pet,
+//         //     owner : owner,
+//         //     vet : vet
+//         // };
+//         alert(JSON.stringify({
+//             appointmentId,
+//             pet,
+//             owner,
+//             vet
+//         }));
+//     });
 
-    $('.edit-icon').click(function () {
-        const appointmentId = this.parentElement.id;
-        const row = this.parentElement.parentElement;
-        const pet = row.children[1].innerText;
-        const owner = row.children[2].innerText;
-        const vet = row.children[3].innerText;
-
-        // const newAppointment = {
-        //     id : appointmentId,
-        //     pet : pet,
-        //     owner : owner,
-        //     vet : vet
-        // };
-        alert(JSON.stringify({
-            appointmentId,
-            pet,
-            owner,
-            vet
-        }));
-    });
-
-
+// DELETE
     $('.delete-icon').click(function () {
         const appointmentId = this.parentElement.id;
 
